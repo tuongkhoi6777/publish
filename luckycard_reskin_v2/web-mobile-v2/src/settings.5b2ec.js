012fc41236e3b0e8a7b5ef0372e5a812dbcb1763
tuongkhoi6777 <tuongkhoi6777@gmail.com>
2021-04-07 11:42:08 +0700

    remove all old build

--- luckycard_reskin_v2/web-mobile-v2/src/settings.5b2ec.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/game.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"69b9a",main:"c2818"}};
