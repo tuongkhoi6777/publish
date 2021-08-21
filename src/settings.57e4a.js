23f07852e4df013e7521937302e6de18e5b10d31
tuongkhoi6777 <tuongkhoi6777@gmail.com>
2021-08-21 13:15:38 +0700

    update

--- src/settings.57e4a.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default","New Group 3"],collisionMatrix:[[true],[false,false]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/game.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"69b9a",main:"eb708"}};
